47802945b3848b23ed3c4afc44b501954d7695f7
Danake Pratiksha <danakepratiksha1020@gmail.com>
2025-01-20 21:37:35 +0530

    added attributes to category class

--- springboot-crud-api/src/main/java/com/example/crud/springboot_crud_api/model/entity/Category.java
+++ springboot-crud-api/src/main/java/com/example/crud/springboot_crud_api/model/entity/Category.java
@@ -1,5 +1,21 @@
 package com.example.crud.springboot_crud_api.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "category")
+@AllArgsConstructor
+@Getter
+@Setter
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long category_id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
 
 }
